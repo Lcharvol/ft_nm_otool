59ee0d99d9ec10d2dee162aa779cd1c48ffe6468
Lcharvol <charvolin0@gmail.com>
2018-09-09 23:33:02 +0200

    Start handle 64

--- src/nm.c
+++ src/nm.c
@@ -1,15 +1,60 @@
 #include "../includes/nm.h"
 
-void	nm(char *pr)
+void						print_output(int nsyms, int symoff, int stroff, char *ptr)
 {
+	int						i;
+	char					*stringtable;
+	struct nlist_64			*array;
 
-};
+	i = 0;
+	array = (void *)ptr + symoff;
+	stringtable = (void *)ptr + stroff;
+	while(i < nsyms)
+	{
+		ft_printf("%s\n", stringtable + array[i].n_un.n_strx);
+		i++;
+	}
+}
 
-int	main(int ac, char **av)
+void						handle_64(char *ptr)
 {
-	int		fd;
-	char	*ptr;
-	struct stat	buf;
+	int						ncmds;
+	int						i;
+	struct mach_header_64	*header;
+	struct load_command		*lc;
+	struct symtab_command	*sym;
+
+	i = 0;
+	header = (struct mach_header_64 *)ptr;
+	ncmds = header->ncmds;
+	lc = (void *)ptr + sizeof(*header);
+	while(i < ncmds)
+	{
+		if(lc->cmd == LC_SYMTAB)
+		{
+			sym = (struct symtab_command *)lc;
+			print_output(sym->nsyms, sym->symoff, sym->stroff, ptr);
+			break ;
+		};
+		lc = (void *)lc + lc->cmdsize;
+		i++;
+	}
+}
+
+void						nm(char *ptr)
+{
+	unsigned int						magic_number;
+
+	magic_number = *(int *) ptr;
+	if(magic_number == MH_MAGIC_64)
+		handle_64(ptr);
+}
+
+int							main(int ac, char **av)
+{
+	int						fd;
+	char					*ptr;
+	struct stat				buf;
 
 	if (ac != 2)
 		return print_usage();
