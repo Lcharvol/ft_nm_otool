1fdd81fdde0ee8ed8a6dc62c9cf7ac6edc425d5d
Lcharvol <charvolin0@gmail.com>
2018-09-16 21:20:03 +0200

    WIP

--- src/otool.c
+++ src/otool.c
@@ -1,23 +1,56 @@
-#include "../includes/nm.h"
+#include "../includes/nm.h"					
+
+void						handle_text_section_fat_arch_64(char *ptr, unsigned long magic)
+{
+	struct segment_command_64	*sc;
+	struct section_64		*sects;
+	struct mach_header_64	*header;
+	uint32_t 					i;
+	uint32_t				ncmds;
+	uint32_t				cmdsize;
+
+	i = 0;
+	header = (struct mach_header_64 *)ptr;
+	ncmds = header->ncmds;
+	ft_printf("ncmds: %d\n", ncmds);
+	sc = (void *)ptr + sizeof(*header);
+	while(i < ncmds)
+	{
+		// if(sc->cmd == LC_SEGMENT_64)
+		// {
+		// 	i = 0;
+		// 	sects = (void *)sc +sizeof(struct segment_command_64);
+		// 	while(i < sc->nsects)
+		// 	{
+		// 		if(ft_strcmp(sects->sectname,SECT_TEXT) == 0)
+		// 			print_text_section_64(sects, ptr);
+		// 		sects = (void *)sects + sizeof(struct section_64);
+		// 		i++;
+		// 	}
+		// };
+		// i++;
+		ft_printf("sc->cmdsize: ", sc->cmdsize);
+		sc = (void *)sc + sc->cmdsize;
+	}
+}
 
 void						handle_fat_arch(char *ptr)
 {
 	struct fat_header	*header;
 	struct fat_arch		*arch;
 	unsigned long		nfat_arch;
-	unsigned long		arch_size;
+	unsigned long		arch_offset;
 	unsigned long		i;
 
 	i = 0;
 	header = (struct fat_header	*)ptr;
 	nfat_arch = header->magic == FAT_MAGIC ? header->nfat_arch :
 		swap_bigendian_littleendian(header->nfat_arch , sizeof(header->nfat_arch ));
-		ft_printf("nfat_arch: %ld\n", nfat_arch);
 	while(i < nfat_arch)
 	{
 		arch = (void *)header + sizeof(struct fat_header) + (i * sizeof(struct fat_arch));
-		arch_size = header->magic == FAT_MAGIC ? arch->size : swap_bigendian_littleendian(arch->size, sizeof(arch->size));
-		ft_printf("arch_size: %d\n", arch_size);
+		arch_offset = header->magic == FAT_MAGIC ? arch->offset : swap_bigendian_littleendian(arch->offset, sizeof(arch->offset));
+		handle_text_section_fat_arch_64((void *)ptr + arch_offset, header->magic);
 		i++;
 	}
 }
